3133623a4c6f4c730078c7d6220c58318bc863a6
Will Shuttleworth <93229335+willshuttleworth@users.noreply.github.com>
2023-05-21 08:46:02 -0400

    still just parsing file

--- main.c
+++ main.c
@@ -9,7 +9,11 @@ typedef struct Header {
     char magic_num[4]; 
     char prg_rom_size; //16kb units
     char chr_rom_size; //8kb units
-    char flags[5];
+    char flags6;
+    char flags7;
+    char flags8;
+    char flags9;
+    char flags10;
     char padding[5];
 }Header;
 
@@ -29,8 +33,24 @@ int main(int argc, char **argv) {
         }
         //read each instruction into an array
         Header h;
-        fread(&h, sizeof(h), 1, file);
+        fread(&h, sizeof(Header), 1, file);
+        printf("magic num: %s\n", h.magic_num);
+        printf("%d %d\n", h.prg_rom_size, h.chr_rom_size);
+        printf("%d\n", h.flags6);
+        printf("%d\n", h.flags7);
+        printf("%d\n", h.flags8);
+        printf("%d\n", h.flags9);
+        printf("%d\n", h.flags10);
+        //checking if trainer is present
+        if((h.flags6 >> 2) | 0) {
+            fseek(file, 512, SEEK_CUR);
+        }
+        printf("%d\n", SEEK_CUR);
         //read actual code lol
+        // just reading first byte
+        char first;
+        fread(&first, sizeof(char), 1, file);
+        printf("first opcode: %x %d\n", first, first);
         fclose(file);
     }
     else {
